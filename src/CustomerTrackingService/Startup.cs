b311c42816fe2d610cf176b285dab3811a347929
Insanityisnice <insanityisnice@hotmail.com>
2017-04-12 14:45:28 -0700

    Add bootstrapper and register loggerFactory for use in Nancy Modules.

--- src/CustomerTrackingService/Startup.cs
+++ src/CustomerTrackingService/Startup.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-
 namespace CustomerTrackingService
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Nancy.Owin;
+
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -22,16 +18,17 @@ namespace CustomerTrackingService
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseOwin(x => x.UseNancy(options => {
+                options.Bootstrapper = new Bootstrapper(loggerFactory);
+                })
+            );
         }
     }
 }
